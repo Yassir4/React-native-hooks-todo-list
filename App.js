b85a8fbf2b82db8bd893be376486290853123b91
Hartani yassir <hartaniyassir@gmail.com>
2018-12-30 10:59:34 +0100

    change design

--- App.js
+++ App.js
@@ -7,12 +7,12 @@
  */
 
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ImageBackground } from 'react-native';
 
 
- const  App = () => {
-  const [value,setValue] = useState('Add a todo')
-  const[todos,setTodos] = useState([])
+const App = () => {
+  const [value, setValue] = useState('')
+  const [todos, setTodos] = useState([])
 
   handleAddTodo = () => {
     console.log('fired')
@@ -20,42 +20,69 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
     setValue('')
   }
 
-    return (
+  return (
+    <ImageBackground style={{ width: '100%', height: '100%', flex: 1 }} source={require('./BackgroudColor.jpg')}>
       <View style={styles.container}>
-        <Text style={styles.welcome}>Todo list</Text>
+        <Text style={{ marginTop: '10%', fontSize: 16, color: 'white' }}>Today</Text>
+        {/* <Text style={styles.welcome}>Todo list</Text> */}
         <TextInput
-          style={{ height: 20, borderColor: 'gray', borderWidth: 1, width: '50%' }}
+          style={{
+            height: 20,
+            borderColor: 'rgb(222,222,222)',
+            borderBottomWidth: 1,
+            width: '100%',
+            minHeight: '7%',
+            marginTop: '5%',
+            fontSize: 25,
+            fontWeight: 'bold',
+            color: 'white',
+            paddingBottom: 15
+
+          }}
+          multiline={true}
           onChangeText={(value) => setValue(value)}
-          placeholder={value}
+          placeholder={'Do it now!'}
+          placeholderTextColor="white"
+          value={value}
         />
-        <Button 
+        <Button
           title="Add"
           onPress={() => handleAddTodo()}
         />
         {
           todos.map((task) => (
-            <Text>{task}</Text>
+            <View style={styles.taskWrapper}>
+              <Text style={styles.task}>{task}</Text>
+            </View>
           ))
         }
       </View>
-    )
+    </ImageBackground>
+  )
 }
 export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    // justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    // backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  taskWrapper: {
+    borderColor: '#D0D0D0',
+    borderBottomWidth: 0.5,
+    width: '100%',
+    paddingBottom: 20,
+    paddingLeft: 10
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  task: {
+    height: 20,
+    borderColor: 'white',
+    borderBottomWidth: 10,
+    // width: '100%',
+    marginTop: '5%',
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: 'white',
+  }
 });
